d94c49fbb40650065d9cc117ae0ca3bf0bb4f91f
appendif <appendif@gmail.com>
2014-08-16 16:18:34 +0200

    Initial commit

---
+++ jimo_sale_report/jimo_sale_report.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+#                                                                               #
+#    OpenERP, Open Source Management Solution                                   #
+#    Copyright (C) 2014 MicroEra (<http://www.microera.it>).                    #
+#                                                                               #
+#    This program is free software: you can redistribute it and/or modify       #
+#    it under the terms of the GNU Affero General Public License as             #
+#    published by the Free Software Foundation, either version 3 of the         #
+#    License, or (at your option) any later version.                            #
+#                                                                               #
+#    This program is distributed in the hope that it will be useful,            #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#    GNU Affero General Public License for more details.                        #
+#                                                                               #
+#    You should have received a copy of the GNU Affero General Public License   #
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.      #
+#                                                                               #
+#################################################################################
+
+from openerp.osv import fields, osv
+from openerp.tools.sql import drop_view_if_exists
+from openerp.addons.decimal_precision import decimal_precision as dp
+
+
+class jimo_sale_report(osv.osv):
+    _name = "jimo.sale.report"
+    _description = "Jimo Sale report"
+    _auto = False
+    _columns = {
+        'date_done': fields.date('Delivery Date', readonly=True),
+        'year': fields.char('Year', size=4, readonly=True),
+        'month':fields.selection([('01','January'), ('02','February'), ('03','March'), ('04','April'),
+            ('05','May'), ('06','June'), ('07','July'), ('08','August'), ('09','September'),
+            ('10','October'), ('11','November'), ('12','December')], 'Month',readonly=True),
+        'day': fields.char('Day', size=128, readonly=True),
+        'saleorder_id':fields.many2one('sale.order', 'Sale Order', readonly=True),
+        'saleperson_id':fields.many2one('res.users', 'Saleperson', readonly=True),
+        'customer_id':fields.many2one('res.partner', 'Customer', readonly=True),
+        'product_id':fields.many2one('product.product', 'Product', readonly=True),
+        'ean13':fields.char('ean13', size=64, readonly=True, help=""),
+        'brand_id':fields.many2one('product.brand', 'Brand', readonly=True),
+        'company_id':fields.many2one('res.company', 'Company', readonly=True),
+        'supplier_id':fields.many2one('res.partner', 'Supplier', readonly=True),
+        'shop_id':fields.many2one('sale.shop', 'Shop', readonly=True),
+        'order_qty':fields.float('Order Qty', digits=(16,2), readonly=True, help=""),
+        'shipped_qty':fields.float('Shipped Qty', digits=(16,2), readonly=True, help=""),
+        'list_price':fields.float('Sale Price', digits=(16,2), readonly=True, help=""),
+        'standard_price':fields.float('Cost Price', digits=(16,2), readonly=True, help=""),
+        'unit_price':fields.float('Unit Price', digits=(16,2), readonly=True, help=""),
+        'total_price':fields.float('Total Price', digits=(16,2), readonly=True, help=""),
+    }
+    
+    def init(self, cr):
+        drop_view_if_exists(cr, 'jimo_sale_report')
+        cr.execute("""
+            create or replace view jimo_sale_report as ( 
+SELECT    
+    sm.id AS id, 
+    to_char(date_trunc('day',sp.date_done), 'YYYY-MM-DD') AS date_done,
+    to_char(date_trunc('day',sp.date_done), 'YYYY') AS year,
+    to_char(date_trunc('day',sp.date_done), 'MM') AS month,
+    to_char(date_trunc('day',sp.date_done), 'DD') AS day,
+    so.id AS saleorder_id,
+    so.user_id AS saleperson_id,
+    so.partner_id AS customer_id,
+    pp.id  AS product_id,
+    pp.ean13 AS ean13,
+    pt.product_brand_id AS brand_id,
+    si.company_id AS company_id,
+    si.name AS supplier_id,
+    so.shop_id AS shop_id,
+    sm.product_qty AS shipped_qty,
+    ( select sum(product_uom_qty) from sale_order_line where order_id=so.id and product_id=sm.product_id ) AS order_qty,
+    pt.list_price AS list_price,
+    pt.standard_price AS standard_price,
+        
+    ( select sum(price_unit * (100.0-discount) / 100.0 * (100.0-so.global_discount_percentage) / 100.0)      
+        from sale_order_line where order_id=so.id and product_id=sm.product_id ) AS unit_price,
+
+    ( select sum(price_unit * product_uom_qty * (100.0-discount) / 100.0 * (100.0-so.global_discount_percentage) / 100.0)      
+        from sale_order_line where order_id=so.id and product_id=sm.product_id ) AS total_price
+    
+    FROM stock_picking sp
+    LEFT JOIN sale_order so           ON (sp.sale_id=so.id)
+    LEFT JOIN stock_move sm           ON (sm.picking_id=sp.id)
+    LEFT JOIN product_product pp      ON (sm.product_id=pp.id)
+         JOIN product_template pt     ON (pp.product_tmpl_id=pt.id)
+    LEFT JOIN product_supplierinfo si ON (pp.id=si.product_id)
+
+    WHERE sp.date_done IS NOT NULL
+      AND sp.type = 'out'
+      AND sp.sale_id IS NOT NULL
+            )""")
+
+jimo_sale_report()
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
