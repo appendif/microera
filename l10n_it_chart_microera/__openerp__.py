6e6ddb53b8aa9e9cc42cc19b01c61c935b4fedca
appendif <appendif@gmail.com>
2014-12-29 14:09:10 +0100

    Microera chart of accounts

---
+++ l10n_it_chart_microera/__openerp__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#                                                                             #
+#   OpenERP, Open Source Management Solution                                  #
+#   Copyright (C) 2014 MicroEra (<http://www.microera.it>).                   #
+#                                                                             #
+#   This program is free software: you can redistribute it and/or modify      #
+#   it under the terms of the GNU Affero General Public License as            #
+#   published by the Free Software Foundation, either version 3 of the        #
+#   License, or (at your option) any later version.                           #
+#                                                                             #
+#   This program is distributed in the hope that it will be useful,           #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#   GNU Affero General Public License for more details.                       #
+#                                                                             #
+#   You should have received a copy of the GNU Affero General Public License  #
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                             #
+###############################################################################
+
+{
+    'name': 'l10n_chart_it_microera',
+    'version': '0.1',
+    'depends': ['base_vat', 'account_chart', 'base_iban'],
+    'author': 'MicroEra srl',
+    'description': """
+Piano dei conti personalizzato per MicroEra srl
+    """,
+    'license': 'AGPL-3',
+    'category': 'Localization/Account Charts',
+    'website': 'http://www.microera.it/',
+    'data': [
+        'data/account.account.template.csv',
+        'data/account.tax.code.template.csv',
+        'account_chart.xml',
+        'data/account.tax.template.csv',
+        'data/account.fiscal.position.template.csv',
+        'l10n_it_chart_microera.xml',
+        ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'images': [],
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
