7066b63542ef4cc582242a314b9e20f7a06d5785
appendif <appendif@gmail.com>
2015-06-07 17:25:05 +0200

    refactoring

--- partner_translation/res_partner.py
+++ partner_translation/res_partner.py
@@ -1,36 +1,69 @@
-# -*- coding: utf-8 -*-
-###############################################################################
-#                                                                             #
-#   OpenERP, Open Source Management Solution                                  #
-#   Copyright (C) 2014 MicroEra (<http://www.microera.it>).                   #
-#                                                                             #
-#   This program is free software: you can redistribute it and/or modify      #
-#   it under the terms of the GNU Affero General Public License as            #
-#   published by the Free Software Foundation, either version 3 of the        #
-#   License, or (at your option) any later version.                           #
-#                                                                             #
-#   This program is distributed in the hope that it will be useful,           #
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of            #
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
-#   GNU Affero General Public License for more details.                       #
-#                                                                             #
-#   You should have received a copy of the GNU Affero General Public License  #
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.     #
-#                                                                             #
-###############################################################################
-
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 MicroEra s.r.l.
+#    (<http://www.microera.it>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 from openerp.osv import osv, fields
 
 
-class res_partner(osv.osv):
+class res_partner(osv.Model):
     _inherit = "res.partner"
 
-    #name = fields.Char(translate=True)
-    #display_name = fields.Char(translate=True)
+    def _display_name_compute(self, cr, uid, ids, name, args, context=None):
+        return super(res_partner, self)._display_name_compute(cr, uid, ids, name, args, context)
+
+    def name_get(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        res = []
+        for record in self.browse(cr, uid, ids, context=context):
+            name = record.name
+
+            if record.foreign_name and record.foreign_name not in name:
+                name = "%s (%s)" % (name, record.foreign_name)
+
+            if record.parent_id and not record.is_company:
+                name = "%s, %s" % (record.parent_name, name)
+            if context.get('show_address_only'):
+                name = self._display_address(cr, uid, record, without_company=True, context=context)
+            if context.get('show_address'):
+                name = name + "\n" + self._display_address(cr, uid, record, without_company=True, context=context)
+            name = name.replace('\n\n', '\n')
+            name = name.replace('\n\n', '\n')
+            if context.get('show_email') and record.email:
+                name = "%s <%s>" % (name, record.email)
+            res.append((record.id, name))
+        return res
+
+    _display_name_store_triggers = {
+        'res.partner': (lambda self,cr,uid,ids,context=None: self.search(cr, uid, [('id','child_of',ids)], context=dict(active_test=False)),
+                        ['parent_id', 'is_company', 'name', 'foreign_name'], 10)
+    }
+
+    _display_name = lambda self, *args, **kwargs: self._display_name_compute(*args, **kwargs)
 
     _columns = {
+        'display_name': fields.function(_display_name, type='char', string='Name', store=_display_name_store_triggers),
         'foreign_name': fields.char('Foreign Name'),
     }
+
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
