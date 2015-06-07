b241aa048e7e4b3d9152f35632b9114010977648
appendif <appendif@gmail.com>
2015-06-07 18:11:48 +0200

    fix documentation

--- partner_translation/__openerp__.py
+++ partner_translation/__openerp__.py
@@ -24,7 +24,7 @@
     'version': '0.1',
     'author': 'MicroEra srl',
     'website': 'http://www.microera.it/',
-    'category': 'Sales',
+    'category': 'Partner',
     'depends': ['base'],
     'demo_xml': [],
     'description': """
@@ -35,7 +35,7 @@ between countries that use a different alphabet
     - Add foreign_name field to partner model
     - Redefine display_name field to partner model
     - Redefine name_get method of partner model appending the value of
-        foreign_name field (if any) after name field
+foreign_name field (if any) after name field
 
   Todo:
     - Use context to switch on/off the new feature
